6380c3250b92685ad5558f66a75bee1347c42adb
Catherine <katerinasheps@gmail.com>
2021-11-07 14:15:14 +0300

    Updated Select Example

--- src/main/scala/testSQL.scala
+++ src/main/scala/testSQL.scala
@@ -1,15 +1,15 @@
+import org.apache.spark.sql.functions.lit
 import org.apache.spark.sql.types.{StringType, StructType}
 import org.apache.spark.sql.{Row, SparkSession}
 
 object testSQL {
 
-  def SelectExample(spark: SparkSession): Unit = {
-    val data = Seq(Row(Row("James","","Smith"),"OH","M"),
-      Row(Row("Anna","Rose",""),"NY","F"),
-      Row(Row("Julia","","Williams"),"OH","F"),
-      Row(Row("Maria","Anne","Jones"),"NY","M"),
-      Row(Row("Jen","Mary","Brown"),"NY","M"),
-      Row(Row("Mike","Mary","Williams"),"OH","M")
+  def AddUpdateSelectExample(spark: SparkSession): Unit = {
+    val dataRows = Seq(Row(Row("James;","","Smith"),"36636","M","3000"),
+      Row(Row("Michael","Rose",""),"40288","M","4000"),
+      Row(Row("Robert","","Williams"),"42114","M","4000"),
+      Row(Row("Maria","Anne","Jones"),"39192","F","4000"),
+      Row(Row("Jen","Mary","Brown"),"","F","-1")
     )
 
     val schema = new StructType()
@@ -17,16 +17,63 @@ object testSQL {
         .add("firstname",StringType)
         .add("middlename",StringType)
         .add("lastname",StringType))
-      .add("state",StringType)
+      .add("dob",StringType)
       .add("gender",StringType)
+      .add("salary",StringType)
 
-    val df = spark.createDataFrame(spark.sparkContext.parallelize(data),schema)
-    df.printSchema()
-    df.show(false)
-    df.select("name").show(false)
-    df.select("name.firstname","name.lastname").show(false)
-    df.select("name.*").show(false)
+    val df2 = spark.createDataFrame(spark.sparkContext.parallelize(dataRows),schema)
+    df2.printSchema()
+    df2.show(false)
+    df2.select("name").show(false)
+    df2.select("name.firstname","name.lastname").show(false)
+    df2.select("name.*").show(false)
+
+    //Change the column data type
+    df2.withColumn("salary",df2("salary").cast("Integer"))
+
+    //Derive a new column from existing
+    val df4=df2.withColumn("CopiedColumn",df2("salary")* -1)
+
+    //Transforming existing column
+    val df5 = df2.withColumn("salary",df2("salary")*100)
+
+    //Renaming a column.
+    val df3=df2.withColumnRenamed("gender","sex")
+    df3.printSchema()
+
+    //Droping a column
+    val df6=df4.drop("CopiedColumn")
+    println(df6.columns.contains("CopiedColumn"))
+
+    //Adding a literal value
+    df2.withColumn("Country", lit("USA")).printSchema()
+
+    //Retrieving
+    df2.show(false)
+    df2.select("name").show(false)
+    df2.select("name.firstname").show(false)
+    df2.select("name.*").show(false)
+
+    import spark.implicits._
+
+    val columns = Seq("name","address")
+    val data = Seq(("Robert, Smith", "1 Main st, Newark, NJ, 92537"), ("Maria, Garcia","3456 Walnut st, Newark, NJ, 94732"))
+    var dfFromData = spark.createDataFrame(data).toDF(columns:_*)
+    dfFromData.printSchema()
+
+    val newDF = dfFromData.map(f=>{
+      val nameSplit = f.getAs[String](0).split(",")
+      val addSplit = f.getAs[String](1).split(",")
+      (nameSplit(0),nameSplit(1),addSplit(0),addSplit(1),addSplit(2),addSplit(3))
+    })
+    val finalDF = newDF.toDF("First Name","Last Name","Address Line1","City","State","zipCode")
+    finalDF.printSchema()
+    finalDF.show(false)
+
+    df2.createOrReplaceTempView("PERSON")
+    spark.sql("SELECT salary*100 as salary, salary*-1 as CopiedColumn, 'USA' as country FROM PERSON").show()
   }
+
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
@@ -35,7 +82,7 @@ object testSQL {
       .config("spark.master", "local")
       .getOrCreate()
 
-    SelectExample(spark)
+    AddUpdateSelectExample(spark)
     spark.stop()
 
   }
